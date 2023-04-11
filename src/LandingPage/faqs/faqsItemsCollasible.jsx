b880929a6d9cb6d5e4cefe85cd8a0297e7ae6628
Shekhorroy100 <shekhorroy100@gmail.com>
2023-04-11 18:46:49 +0530

    Faq ui fixed

--- src/LandingPage/faqs/faqsItemsCollasible.jsx
+++ src/LandingPage/faqs/faqsItemsCollasible.jsx
@@ -1,11 +1,13 @@
 import styles from "./faqs.module.css"
 import React, { useState } from "react";
 import Image from "next/image";
+import {isMobile} from "@/configs/utils";
 const DownArrowVector =  "./images/chevron-down.svg";
 const UpArrowVector = "./images/chevron-up1.svg";
 
 const FaqCollapsible = (props) => {
   const [open, setOPen] = useState(false);
+  const _isMobile = isMobile()
 
   const toggle = () => {
     setOPen(!open);
@@ -19,7 +21,7 @@ const FaqCollapsible = (props) => {
             </h4>
             <div className={styles.Faqs_ContainerQuestionArrowVector}>
                 {
-                    !open?<Image src={DownArrowVector} alt="down vector" height={20} width={20}/>:<Image src={UpArrowVector} alt="up vector" height={20} width={20}/>
+                    !open? <Image src={DownArrowVector} alt="down vector" height={20} width={20}/>:<Image src={UpArrowVector} alt="up vector" height={20} width={20}/>
                 }
             </div>
 
