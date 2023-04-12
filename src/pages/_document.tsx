884f2e444b499d906505347bd07c17dff1fd8d7e
Shekhorroy100 <shekhorroy100@gmail.com>
2023-04-12 16:40:59 +0530

    Google tag added

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,18 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+          {/*google tag*/}
+          <script
+            dangerouslySetInnerHTML={{
+                __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                      })(window,document,'script','dataLayer','GTM-TMWSSRS');`
+            }}
+          />
+          {/*google tag*/}
+
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
@@ -11,10 +23,24 @@ export default function Document() {
               rel="stylesheet"
           />
           <link rel="icon" href="/favicon.ico" />
+
+          {/*google tag*/}
+          <script dangerouslySetInnerHTML={{__html: `async src="https://www.googletagmanager.com/gtag/js?id=AW-11053018943"`}} />
+          <script dangerouslySetInnerHTML={{
+              __html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'AW-11053018943');`
+          }} />
+          {/*google tag*/}
       </Head>
       <body>
         <Main />
         <NextScript />
+        {/*Google Tag Manager (noscript)*/}
+        <noscript
+            dangerouslySetInnerHTML={{
+                __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TMWSSRS" height="0" width="0" style="display:none;visibility:hidden" />`,
+            }}
+        />
+        {/*End Google Tag Manager (noscript)*/}
       </body>
     </Html>
   );
