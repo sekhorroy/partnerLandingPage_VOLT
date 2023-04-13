54d9880bb73748752fe0179a7af3a33d8c24fc0d
Shekhorroy100 <shekhorroy100@gmail.com>
2023-04-13 17:46:33 +0530

    added title and description for the contact us page

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -6,10 +6,10 @@ export default function Contactus() {
     return (
        <>
            <Head>
-               <title>My page title</title>
+               <title>Volt Money | Contact us</title>
                <meta
                    name="description"
-                   content="Help your clients meet short term cash needs by redeeming pledging mutual funds | Stop redemptions & grow mutual fund AUM"
+                   content="India’s first app to offer instant online credit line against securities, making credit easily accessible to millions of Indians right when they need it in under 5 minutes."
                />
            </Head>
            <ContactUsPage />
