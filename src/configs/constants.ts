1c3ab832e12a7a87f25d03595fb0dc1d590cd89c
Shekhorroy100 <shekhorroy100@gmail.com>
2023-04-13 19:12:40 +0530

    Ranjan feedback changes

--- src/configs/constants.ts
+++ src/configs/constants.ts
@@ -3,7 +3,7 @@ export enum BUILD_TYPE {
   PARTNER_STAGING = "PARTNER_STAGING",
 }
 export const getBuildType = () => {
-  const buildType = BUILD_TYPE.PARTNER_STAGING;
+  const buildType = BUILD_TYPE.PARTNER_PRODUCTION;
   return buildType;
 };
 // export const PartnerLink =
